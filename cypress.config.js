1c3663765a1918cc1e51751780adca6f62730c1c
Hamxa-Khalid <hamxi8381@gmail.com>
2023-07-05 21:34:36 +0500

    Just commiting the previouse code

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,11 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "https://int.sandandsky.com",
-    specPattern: "cypress/testFile/**/*.cy.js",
+    baseUrl: 'https://int.sandandsky.com',
+    specPattern: 'cypress/testFile/**/*.cy.js',
   },
 });
